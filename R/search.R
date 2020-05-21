277045fa864bedb7fe5a9a2d3d994e5059ac64e3
Arles <arles@curta.red>
2020-05-21 11:05:36 -0300

    Update search.R

--- R/search.R
+++ R/search.R
@@ -38,7 +38,7 @@
 #' \dontrun{
 #' search_spotify('radiohead', 'artist')
 #' }
-search_spotify <- function(q, type = c('album', 'artist', 'playlist', 'track'), market = NULL, limit = 20, offset = 0, include_external = NULL, authorization = get_spotify_access_token(), include_meta_info = FALSE) {
+search_spotify <- function(q, type = c('album', 'artist', 'playlist', 'track', 'show', 'episode'), market = NULL, limit = 20, offset = 0, include_external = NULL, authorization = get_spotify_access_token(), include_meta_info = FALSE) {
 
     base_url <- 'https://api.spotify.com/v1/search'
 
